3ce5804b3911a53df3c41da45afe4dca109f6c40
Nestor Pylat <nestorpylat33@gmail.com>
2021-11-15 21:27:33 +0200

    Task Lecture 1

---
+++ src/Page 1/customButton.js
@@ -0,0 +1,18 @@
+class CustomButton extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const button = document.createElement('button');
+    button.setAttribute('class', 'custom-button');
+    button.addEventListener('click', () => {
+      confirm('do you want to buy it? ');
+    });
+    shadow.appendChild(button);
+
+    button.textContent = 'Change image';
+  }
+}
+
+customElements.define('custom-button', CustomButton);
