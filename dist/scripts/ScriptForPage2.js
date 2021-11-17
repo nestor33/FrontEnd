c6a521ffdf9eb6a485df6af999d4c13ebc15fffe
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-17 12:29:48 +0200

    task frontEnd

--- dist/scripts/ScriptForPage2.js
+++ dist/scripts/ScriptForPage2.js
@@ -1,15 +1,17 @@
+//get values from form
 function submitForm(e, form) {
   e.preventDefault();
   var newObject = {
     Name: form.formName.value,
     Options: form.formOptions.value,
     Description: form.formDescription.value,
-    imgSrc: './../../images/cactus.svg',
+    imgSrc: "./../../images/cactus.svg",
   };
 
   console.log(form.formName.value);
   console.log(form.formOptions.value);
   console.log(form.formDescription.value);
 
-  sessionStorage.setItem('newObject', JSON.stringify(newObject));
+  //put to session storage
+  sessionStorage.setItem("newObject", JSON.stringify(newObject));
 }
--- dist/scripts/scriptForPage1.js
+++ dist/scripts/scriptForPage1.js
@@ -1,36 +1,42 @@
-var filterButtonName1 = document.querySelector('.Name1');
-var filterButtonName2 = document.querySelector('.Name2');
+var filterButtonName1 = document.querySelector(".Name1");
+var filterButtonName2 = document.querySelector(".Name2");
 var allProducts = document.querySelectorAll(
-  '.div_navbarAndObjects_maimDivObjects_allObjectsDiv_oneObject'
+  ".div_navbarAndObjects_maimDivObjects_allObjectsDiv_oneObject"
 );
 
-//Filter
-filterButtonName1.addEventListener('click', () => {
+//Filter Name1
+filterButtonName1.addEventListener("click", FilterName1);
+
+function FilterName1() {
   allProducts.forEach((product) => {
     product.hidden = false;
     if (product.id != 1) {
       product.hidden = true;
     }
   });
-});
-filterButtonName2.addEventListener('click', () => {
+}
+
+//Filter Name2
+filterButtonName2.addEventListener("click", FilterName2);
+
+function FilterName2() {
   allProducts.forEach((product) => {
     product.hidden = false;
     if (product.id != 2) {
       product.hidden = true;
     }
   });
-});
+}
 
 //Add to storage
-document.querySelectorAll('.addButton').forEach((button) => {
-  button.addEventListener('click', (event) => {
-    let buttonId = event.target.parentNode.getAttribute('id');
+document.querySelectorAll(".addButton").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let buttonId = event.target.parentNode.getAttribute("id");
     console.log(buttonId);
     products.forEach((element) => {
       if (element.id == buttonId) {
-        event.target.innerHTML = 'Added';
-        sessionStorage.setItem('id', element.id);
+        event.target.innerHTML = "Added";
+        sessionStorage.setItem("id", element.id);
       }
     });
   });
@@ -44,18 +50,18 @@ function AddProduct() {
   let div =
     "<div class='div_navbarAndObjects_maimDivObjects_allObjectsDiv_oneObject' id='7'>";
   let h2img =
-    '<h2>' +
+    "<h2>" +
     array.Name +
     "</h2> <img src='./../../images/cactus.svg' class='' alt='' /> <p>Options:" +
     array.Options +
-    '</p><p>Description:' +
+    "</p><p>Description:" +
     array.Description +
-    '</p>';
+    "</p>";
   let buttons =
     "<button type='button' class='addButton'>Add</button> <custom-button></custom-button></div>";
   let list = div + h2img + buttons;
   let currentEl = document.querySelector(
-    '.div_navbarAndObjects_maimDivObjects_allObjectsDiv'
+    ".div_navbarAndObjects_maimDivObjects_allObjectsDiv"
   );
   console.log(currentEl);
   currentEl.innerHTML += list;
@@ -63,10 +69,10 @@ function AddProduct() {
 
 //Counter
 let i = 0;
-let counter = document.querySelector('.counter');
-let addButtons = document.querySelectorAll('.addButton');
+let counter = document.querySelector(".counter");
+let addButtons = document.querySelectorAll(".addButton");
 addButtons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     if (sessionStorage.length != null) {
       i = sessionStorage.length;
       counter.textContent = `Counter: ${i.toString()}`;
